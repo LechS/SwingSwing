45f6b0df836925cd6bbc646ba274681d5d061598
lech.sawon <lech.sawon@silk-sh.pl>
2016-10-06 19:29:05 +0200

    facebook sdk install

--- src/FacebookBundle/Controller/DefaultController.php
+++ src/FacebookBundle/Controller/DefaultController.php
@@ -2,21 +2,23 @@
 
 namespace FacebookBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/success/login", name="loginSucces")
-     * @Security()
      */
     public function facebookLoginSuccessAction(Request $request)
     {
+        if(!$loggedUser = $this->getUser()){
+            throw new Exception('No access');
+        }
 
-
+        die('asdasda');
 
         return $this->render('FacebookBundle:Default:index.html.twig');
     }
